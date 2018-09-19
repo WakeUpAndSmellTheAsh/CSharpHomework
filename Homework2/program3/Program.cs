707b2f44ac368d1a3c0275bd09a8ea412b62ebc6
Q Moon <375005603@qq.com>
2018-09-19 20:28:07 +0800

    23行控制语句循环+1

--- Homework2/program3/Program.cs
+++ Homework2/program3/Program.cs
@@ -16,7 +16,7 @@ namespace program3
         {
             int[] num = new int[101];        //（开始i=2)如果i是合数，num[i]=1；否则为0
             int primeCount = 0;              // 记录 素数个数
-            int[] prime = new int[101];      // 记录 素数 
+            int[] prime = new int[101];      // 存 素数 
 
             findPrime();
             //输出素数
